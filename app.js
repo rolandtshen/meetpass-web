f5b54606a2908f2955872de7d8efa8dc4030750d
Roland Shen <rolandtshen@gmail.com>
2018-12-07 21:43:53 +0100

    Working themes

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const path = require('path');
 
 const handlebars = require('express-handlebars');
 
-app.engine('.hbs', handlebars({ extname: '.hbs' }));
+app.engine('.hbs', handlebars({
+  extname: '.hbs'
+}));
 
 app.set("PORT", PORT);
 
@@ -14,26 +16,78 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
-app.get('/', function (req, res) {
-    res.render("index", {title: "MeetPass"});
+app.get('/', function(req, res) {
+  res.render("index", {
+    title: "MeetPass"
+  });
 });
 
-app.get('/:cID', function (req, res) {
+app.get('/:cID', function(req, res) {
   var cID = req.params.cID;
 
   request(`https://meetpass-server.herokuapp.com/cards/1/${cID}`, function(error, response, body) {
-    if(!error && response.statusCode == 200) {
+    if (!error && response.statusCode == 200) {
       var card = JSON.parse(body);
-      res.render("card", { title: `MeetPass - ${card.cardName}`, accounts: card.accounts, card: card});
+      var foreground;
+      var background;
+      switch (card.themeId) {
+        case "0":
+          foreground = "#000000";
+          background = "#FFFFFF";
+          break;
+        case "1":
+          foreground = "#4D0722";
+          background = "#FFD8E4";
+          break;
+        case "2":
+          foreground = "#823E37";
+          background = "#FFE1D1";
+          break;
+        case "3":
+          foreground = "#432905";
+          background = "#FFEBB6";
+          break;
+        case "4":
+          foreground = "#082408";
+          background = "#C5FDC7";
+          break;
+        case "5":
+          foreground = "#2E3A5E";
+          background = "#CAF1FE";
+          break;
+        case "6":
+          foreground = "#2E3A5E";
+          background = "#CDDFFD";
+          break;
+        case "7":
+          foreground = "#122A5D";
+          background = "#99C5FF";
+          break;
+        case "8":
+          foreground = "#391B4C";
+          background = "#EFDFFC";
+          break;
+        default:
+          foreground = "#000000";
+          background = "#FFFFFF";
+          break;
+      }
+      res.render("card", {
+        title: `MeetPass - ${card.cardName}`,
+        accounts: card.accounts,
+        card: card,
+        background: background,
+        foreground: foreground
+      });
     }
     else {
-        //Make a 404 page and insert here
-        res.send("This card does not exist.");
+      //Make a 404 page and insert here
+      res.send("This card does not exist.");
     }
   });
 });
 
-app.listen(app.get('PORT'), function () {
-    console.log('Express started on http://localhost:' +
-        app.get('PORT'));
+app.listen(app.get('PORT'), function() {
+  console.log('Express started on http://localhost:' +
+    app.get('PORT'));
 });
