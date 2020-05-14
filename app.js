c5e6d125f2bb2cbad4d1e03c9165e28965167b35
Roland <rolandtshen@gmail.com>
2020-05-14 15:19:51 -0700

    remove ssl redir

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, 'assets')));
-app.use(sslRedirect());
+// app.use(sslRedirect());
 app.use(favicon(path.join(__dirname, 'assets', 'img', 'branding', 'favicon.ico')))
 
 
